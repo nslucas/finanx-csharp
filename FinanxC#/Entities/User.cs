e6f12ac4d7cd4e07bc60b612fe39253a818f5cbd
Lucas Nunes <lucasnunes.santos088@gmail.com>
2024-04-04 23:55:29 -0300

    feat: create User entity

---
+++ FinanxC#/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Finanx.Entities
+{
+    public class User
+    {
+        public User(String id, String name, String email, String password) 
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+        }
+
+        private String Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+    }
+}
--- FinanxC#/obj/Debug/net8.0/FinanxC.AssemblyInfo.cs
+++ FinanxC#/obj/Debug/net8.0/FinanxC.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("FinanxC#")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("FinanxC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf6fd76432e65aa0cc49bb6fb8158598963cbb07")]
-[assembly: System.Reflection.AssemblyProductAttribute("FinanxC#")]
-[assembly: System.Reflection.AssemblyTitleAttribute("FinanxC#")]
+[assembly: System.Reflection.AssemblyProductAttribute("FinanxC")]
+[assembly: System.Reflection.AssemblyTitleAttribute("FinanxC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
